64edc32644b866183eab5e4e1b41a6bb4a1ef280
korpuus <korpus.kuba@gmail.com>
2024-02-07 18:57:32 +0100

    added button, to do: addeventlistener

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+//button with prompt
+const popupBtn = document.createElement('button');
+popupBtn.setAttribute('id', 'popupbtn');
+document.body.append(popupBtn);
+popupBtn.textContent = 'Grid size';
 
+
+function numberOfSquares(x){
+  x = +prompt('Enter a number of squares per side: ');
+};
+
+//creating divs
 const containerDiv = document.createElement('div');
 containerDiv.setAttribute('id', 'container-div');
 document.body.appendChild(containerDiv);
 
-
+//loop for grid dimensions
 for (let i = 0; i < 16; i++) {
   const squareDiv = document.createElement('div');
   containerDiv.appendChild(squareDiv);
@@ -13,6 +24,14 @@ for (let i = 0; i < 16; i++) {
 }
 
 
+//hovering effect
+const squareDivs = document.querySelectorAll('.square-div');
 
+squareDivs.forEach(squareDiv => {
+  squareDiv.addEventListener('mouseover', () => {
+    squareDiv.style.background = 'blue';
+  });
+})
 });
+
 
