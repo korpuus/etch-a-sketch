33cd042821b09bc80e415dfa036b6e1176b806aa
korpuus <korpus.kuba@gmail.com>
2024-02-07 20:58:29 +0100

    work in progress on button event listener

--- script.js
+++ script.js
@@ -10,23 +10,33 @@ popupBtn.setAttribute('id', 'popup-btn');
 buttonDiv.appendChild(popupBtn);
 popupBtn.textContent = 'Grid size';
 
-
-
-function numberOfSquares(){
-  return +prompt('Enter number of squares per side: ');
-};
-
 //creating divs for grid screen
 const containerDiv = document.createElement('div');
 containerDiv.setAttribute('id', 'container-div');
 document.body.appendChild(containerDiv);
 
 //loop for grid dimensions
-for (let i = 0; i < 16; i++) {
-  const squareDiv = document.createElement('div');
-  containerDiv.appendChild(squareDiv);
-  squareDiv.setAttribute('class', 'square-div');
+function squaresPerSide(squares = 16) {
+  for (let i = 0; i < squares; i++) {
+    const squareDiv = document.createElement('div');
+    containerDiv.appendChild(squareDiv);
+    squareDiv.setAttribute('class', 'square-div');
+  }
 }
+squaresPerSide();
+//button eventlistener
+
+popupBtn.addEventListener('click', () => {
+
+  let squaresNumber = +prompt('Enter number of squares per side: ');
+  if (squaresNumber <= 100 && 
+    squaresNumber >= 4 && 
+    squaresNumber % 2 === 0) {
+      squaresPerSide(squares = squaresNumber);
+  } else {
+    return alert('It should be an even number, and maximum is 100');
+  }
+});
 
 
 //hovering effect
